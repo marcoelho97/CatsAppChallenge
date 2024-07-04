78e66696dfb54d9435dfe9c1296d797103a20088
Marco Coelho <marcoelho97@gmail.com>
2024-07-04 11:39:25 +0100

    Adding the search filter to the GetAllBreeds query in BreedDao

--- app/src/main/java/com/example/catsappchallenge/data/dao/BreedDao.kt
+++ app/src/main/java/com/example/catsappchallenge/data/dao/BreedDao.kt
@@ -1,5 +1,6 @@
 package com.example.catsappchallenge.data.dao
 
+import android.util.Log
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Upsert
@@ -17,8 +18,10 @@ interface BreedDao {
     @Query("UPDATE breed SET favourite = :favourite WHERE id = :breedId")
     suspend fun updateFavouriteByBreedId(breedId: String, favourite: Boolean)
 
-    @Query("SELECT id, name, image, favourite FROM breed WHERE (:filterFavourite IS NULL OR favourite = :filterFavourite)")
-    fun getAllBreeds(filterFavourite: Boolean?): List<BreedListDTO>
+    @Query("SELECT id, name, image, favourite FROM breed " +
+            "WHERE (:filterFavourite IS NULL OR favourite = :filterFavourite) " +
+            "AND name LIKE :searchFilter")
+    fun getAllBreeds(searchFilter: String = "%", filterFavourite: Boolean?): List<BreedListDTO>
 
     @Query("SELECT * FROM breed WHERE id = :breedId")
     fun getBreedById(breedId: String): Breed
