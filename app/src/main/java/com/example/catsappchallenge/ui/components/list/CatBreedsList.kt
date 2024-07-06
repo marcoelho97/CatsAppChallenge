778d8fde5b6b797490b2a30b32f52c3bec2aa6e0
Marco Coelho <marcoelho97@gmail.com>
2024-07-06 17:05:13 +0100

    LightGray color on breeds without images

--- app/src/main/java/com/example/catsappchallenge/ui/components/list/CatBreedsList.kt
+++ app/src/main/java/com/example/catsappchallenge/ui/components/list/CatBreedsList.kt
@@ -1,5 +1,6 @@
 package com.example.catsappchallenge.ui.components.list
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -67,7 +69,9 @@ fun CatCard(navController: NavController, breed: BreedListDTO, breedViewModel: B
             },
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box {
+        Box(
+            modifier = Modifier.background(Color.LightGray)
+        ) {
             ImageBox(
                 breedImage = breed.image,
                 modifier = Modifier.size(128.dp)
